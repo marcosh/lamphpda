6c97a6b51de33bde9299640a375bd0b5f7df60c7
Marco Perone <pasafama@gmail.com>
2020-04-03 17:26:36 +0200

    test map for Writer

--- spec/WriterSpec.php
+++ spec/WriterSpec.php
@@ -9,10 +9,18 @@ use Marcosh\LamPHPda\Writer;
 
 describe('Writer', function () {
     it('exec returns the inner pair', function () {
-        $writer = Writer::writer(Pair::pair(42, "a"));
+        $writer = Writer::writer(Pair::pair(42, 'a'));
 
         $result = $writer->exec();
 
-        expect($result)->toEqual(Pair::pair(42, "a"));
+        expect($result)->toEqual(Pair::pair(42, 'a'));
+    });
+
+    it ('maps the second component', function () {
+        $writer = Writer::writer(Pair::pair(42, 'a'));
+
+        $result = $writer->map(fn($value) => $value * 2);
+
+        expect($result)->toEqual(Writer::writer(Pair::pair(84, 'a')));
     });
 });

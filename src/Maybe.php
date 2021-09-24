85d9a635ce798afdf500aaebfc0ae729c54d9927
marcosh <pasafama@gmail.com>
2021-09-24 18:02:14 +0200

    use functor instance on Maybe

--- src/Maybe.php
+++ src/Maybe.php
@@ -4,22 +4,34 @@ declare(strict_types=1);
 
 namespace Marcosh\LamPHPda;
 
+use Marcosh\LamPHPda\Brand\MaybeBrand;
+use Marcosh\LamPHPda\HK\HK1;
+use Marcosh\LamPHPda\Typeclass\Functor;
+
 /**
  * @template A
+ * @implements HK1<MaybeBrand, A>
  */
-final class Maybe
+final class Maybe implements HK1
 {
-    /** @var bool */
+    /**
+     * @var bool
+     *
+     * @psalm-readonly
+     */
     private $isJust;
 
     /**
      * @var A|null
+     *
+     * @psalm-readonly
      */
     private $value = null;
 
     /**
      * @param bool $isJust
      * @param A|null $value
+     *
      * @psalm-mutation-free
      */
     private function __construct(bool $isJust, $value = null)
@@ -32,6 +44,7 @@ final class Maybe
      * @template B
      * @param B $value
      * @return Maybe<B>
+     *
      * @psalm-pure
      */
     public static function just($value): Maybe
@@ -40,6 +53,9 @@ final class Maybe
     }
 
     /**
+     * @template B
+     * @return Maybe<B>
+     *
      * @psalm-pure
      */
     public static function nothing(): Maybe
@@ -49,9 +65,23 @@ final class Maybe
 
     /**
      * @template B
+     * @param HK1<MaybeBrand, B> $b
+     * @return Maybe<B>
+     *
+     * @psalm-pure
+     */
+    public static function fromBrand(HK1 $b): Maybe
+    {
+        /** @var Maybe $b */
+        return $b;
+    }
+
+    /**
+     * @template B
      * @param B $ifNothing
      * @param callable(A): B $ifJust
      * @return B
+     *
      * @psalm-mutation-free
      */
     public function eval(
@@ -64,5 +94,21 @@ final class Maybe
         }
 
         return $ifNothing;
+    }
+
+    /**
+     * @template B
+     * @param Functor<MaybeBrand> $functor
+     * @param callable(A): B $f
+     * @return Maybe<B>
+     *
+     * @psalm-mutation-free
+     */
+    public function map(Functor $functor, callable $f): Maybe
+    {
+        /** @var Maybe<B> $maybeB */
+        $maybeB = $functor->map($f, $this);
+
+        return $maybeB;
     }
 }

8891d7698f3c87eb48490258e87a77ff1051ced7
marcosh <pasafama@gmail.com>
2021-09-24 18:02:31 +0200

    missing pieces

---
+++ src/Brand/MaybeBrand.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\Brand;
+
+use Marcosh\LamPHPda\Maybe;
+
+/**
+ * @implements Brand<Maybe>
+ */
+final class MaybeBrand implements Brand
+{
+}
---
+++ src/HK/HK1.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\HK;
+
+use Marcosh\LamPHPda\Brand\Brand;
+
+/**
+ * @template F of Brand
+ * @template A
+ */
+interface HK1
+{
+}

18eba875cc914295caba25c273129714348f687b
marcosh <pasafama@gmail.com>
2021-09-24 18:01:43 +0200

    define Functor and Apply

---
+++ src/Typeclass/Apply.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\Typeclass;
+
+use Marcosh\LamPHPda\Brand\Brand;
+use Marcosh\LamPHPda\HK\HK1;
+
+/**
+ * @template F of Brand
+ * @extends Functor<F>
+ *
+ * @psalm-immutable
+ */
+interface Apply extends Functor
+{
+    /**
+     * @template A
+     * @template B
+     * @param HK1<F, callable(A): B> $f
+     * @param HK1<F, A> $a
+     * @return HK1<F, B>
+     */
+    public function apply(HK1 $f, HK1 $a): HK1;
+}

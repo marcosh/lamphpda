5593f8260d2a2f76c0944beb99b4886298fea75d
marcosh <pasafama@gmail.com>
2021-10-15 23:28:59 +0200

    define lift2 in ExtraApply class

---
+++ src/Typeclass/Extra/ExtraApply.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\Typeclass\Extra;
+
+use Marcosh\LamPHPda\Brand\Brand;
+use Marcosh\LamPHPda\HK\HK1;
+use Marcosh\LamPHPda\Typeclass\Apply;
+
+/**
+ * @template F of Brand
+ *
+ * @psalm-immutable
+ */
+final class ExtraApply
+{
+    private Apply $apply;
+
+    public function __construct(Apply $apply)
+    {
+        $this->apply = $apply;
+    }
+
+    /**
+     * @template A
+     * @template B
+     * @template C
+     * @param callable(A, B): C $f
+     * @param HK1<F, A> $a
+     * @param HK1<F, B> $b
+     * @return HK1<F, C>
+     *
+     * @psalm-suppress InvalidReturnType
+     */
+    public function lift2(callable $f, HK1 $a, HK1 $b): HK1
+    {
+        $curriedF =
+            /**
+             * @param A $ca
+             * @return callable(B): C
+             */
+            fn ($ca) =>
+                /**
+                 * @param B $cb
+                 * @return C
+                 */
+            fn ($cb) => $f($ca, $cb);
+
+        /** @psalm-suppress InvalidReturnStatement */
+        return $this->apply->apply(
+            $this->apply->map($curriedF, $a),
+            $b
+        );
+    }
+}

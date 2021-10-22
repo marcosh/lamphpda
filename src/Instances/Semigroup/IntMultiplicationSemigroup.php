c919010b7a7503563578f72454bda39ddd216ca9
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-10-22 21:42:10 +0200

    Add `IntMultiplicationSemigroup` instance.

---
+++ src/Instances/Semigroup/IntMultiplicationSemigroup.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\Instances\Semigroup;
+
+use Marcosh\LamPHPda\Typeclass\Semigroup;
+
+/**
+ * @template A
+ *
+ * @implements Semigroup<A>
+ *
+ * @psalm-immutable
+ */
+final class IntMultiplicationSemigroup implements Semigroup
+{
+    /**
+     * @param A $a
+     * @param A $b
+     * @return A
+     *
+     * @psalm-pure
+     */
+    public function append($a, $b)
+    {
+        return $a * $b;
+    }
+}

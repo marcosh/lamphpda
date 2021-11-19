eff54aa5a6621902e3faf231263354f5212bb2c7
marcosh <pasafama@gmail.com>
2021-11-19 15:33:30 +0100

    define string concatenation monoid

---
+++ src/Instances/String/ConcatenationMonoid.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\Instances\String;
+
+use Marcosh\LamPHPda\Typeclass\Monoid;
+
+/**
+ * @implements Monoid<string>
+ *
+ * @psalm-immutable
+ */
+final class ConcatenationMonoid implements Monoid
+{
+    public function mempty(): string
+    {
+        return '';
+    }
+
+    /**
+     * @param string $a
+     * @param string $b
+     * @return string
+     */
+    public function append($a, $b): string
+    {
+        return $a . $b;
+    }
+}

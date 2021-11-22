d723872824753fcc7d6a82ea0d1b7ad7ce2ce6db
marcosh <pasafama@gmail.com>
2021-11-22 09:49:08 +0100

    HK2 interface

---
+++ src/HK/HK2.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\HK;
+
+use Marcosh\LamPHPda\Brand\Brand;
+
+/**
+ * @template F of Brand
+ * @template A
+ * @template B
+ */
+interface HK2
+{
+}

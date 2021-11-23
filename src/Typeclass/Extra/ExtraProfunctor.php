4630e97fd727e3914e378e728706de2c5857daba
marcosh <pasafama@gmail.com>
2021-11-23 10:13:45 +0100

    define lmap and rmap for profunctors

---
+++ src/Typeclass/Extra/ExtraProfunctor.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\Typeclass\Extra;
+
+use Marcosh\LamPHPda\Brand\Brand;
+use Marcosh\LamPHPda\HK\HK2;
+use Marcosh\LamPHPda\Typeclass\Profunctor;
+
+/**
+ * @template F of Brand
+ *
+ * @psalm-immutable
+ */
+final class ExtraProfunctor
+{
+    /** @var Profunctor<F> */
+    private Profunctor $profunctor;
+
+    /**
+     * @param Profunctor<F> $profunctor
+     */
+    private function __construct(Profunctor $profunctor)
+    {
+        $this->profunctor = $profunctor;
+    }
+
+    /**
+     * @template A
+     * @template B
+     * @template C
+     * @param callable(A): B $f
+     * @param HK2<F, C, A> $hk
+     * @return HK2<F, C, B>
+     */
+    public function rmap(callable $f, HK2 $hk): HK2
+    {
+        return $this->profunctor->diMap(
+            /**
+             * @param C $c
+             * @return C
+             */
+            fn($c) => $c,
+            $f,
+            $hk
+        );
+    }
+
+    /**
+     * @template A
+     * @template B
+     * @template C
+     * @param callable(A): B $f
+     * @param HK2<F, B, C> $hk
+     * @return HK2<F, A, C>
+     */
+    public function lmap(callable $f, HK2 $hk): HK2
+    {
+        return $this->profunctor->diMap(
+            $f,
+            /**
+             * @param C $c
+             * @return C
+             */
+            fn($c) => $c,
+            $hk
+        );
+    }
+}

c1eb880ad8160fe02bcb8470e5fdabc56001573d
marcosh <pasafama@gmail.com>
2021-11-23 10:13:31 +0100

    profunctor typeclass

---
+++ src/Typeclass/Profunctor.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\Typeclass;
+
+use Marcosh\LamPHPda\Brand\Brand;
+use Marcosh\LamPHPda\HK\HK2;
+
+/**
+ * @template F of Brand
+ *
+ * @psalm-immutable
+ */
+interface Profunctor
+{
+    /**
+     * @template A
+     * @template B
+     * @template C
+     * @template D
+     * @param callable(A): B $f
+     * @param callable(C): D $g
+     * @param HK2<F, B, C> $a
+     * @return HK2<F, A, D>
+     */
+    public function diMap(callable $f, callable $g, HK2 $a): HK2;
+}

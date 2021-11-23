1dd0a7509760b461dfc7fe4a792dc246510d7c57
marcosh <pasafama@gmail.com>
2021-11-23 11:57:33 +0100

    list concatenation monoid

---
+++ src/Instances/ListL/ConcatenationMonoid.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\Instances\ListL;
+
+use Marcosh\LamPHPda\Typeclass\Monoid;
+
+/**
+ * @template A
+ * @implements Monoid<list<A>>
+ *
+ * @psalm-immutable
+ */
+final class ConcatenationMonoid implements Monoid
+{
+    /**
+     * @return list<A>
+     */
+    public function mempty(): array
+    {
+        return [];
+    }
+
+    /**
+     * @param list<A> $a
+     * @param list<A> $b
+     * @return list<A>
+     */
+    public function append($a, $b)
+    {
+        return array_merge($a, $b);
+    }
+}

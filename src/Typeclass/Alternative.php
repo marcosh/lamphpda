2bca6635bf3e179e070c6ea9fb59b7d5f1bf69e1
marcosh <pasafama@gmail.com>
2021-12-07 17:01:03 +0100

    define alternative typeclass

---
+++ src/Typeclass/Alternative.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\LamPHPda\Typeclass;
+
+use Marcosh\LamPHPda\Brand\Brand;
+use Marcosh\LamPHPda\HK\HK1;
+
+/**
+ * @template F of Brand
+ *
+ * @extends Applicative<F>
+ *
+ * @psalm-immutable
+ */
+interface Alternative extends Applicative
+{
+    /**
+     * @template A
+     * @return HK1<F, A>
+     */
+    public function empty();
+
+    /**
+     * @template A
+     * @param HK1<F, A> $a
+     * @param HK1<F, A> $b
+     * @return HK1<F, A>
+     */
+    public function alt($a, $b);
+}

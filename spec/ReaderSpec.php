78a00cda885e391521c30659e9e1c8294eef58d0
marcosh <pasafama@gmail.com>
2021-12-10 09:19:37 +0100

    fix reader tests

--- spec/ReaderSpec.php
+++ spec/ReaderSpec.php
@@ -5,27 +5,27 @@ declare(strict_types=1);
 use Marcosh\LamPHPda\Reader;
 
 describe('Reader', function () {
-    it('apply', function () {
-        $addTwo = static fn (int $x): int => $x + 2;
-
-        $result = Reader::reader(fn () => $addTwo)->apply(Reader::reader(fn (): int => 10))->runReader(50);
-
-        expect($result)->toEqual(12);
-    });
-
-    it('bind', function () {
-        $bind = static fn (int $x) => Reader::reader(fn () => 5)->map(static fn (int $y): int => $x * $y);
-
-        $result = Reader::reader(fn (): int => 2)->bind($bind)->runReader('foobar');
-
-        expect($result)->toEqual(10);
-    });
-
     it('map', function () {
         $mapper = static fn (int $x): int => $x * 5;
 
-        $result = Reader::reader(fn (): int => 2)->map($mapper)->runReader(20);
+        $result = Reader::reader(fn (int $x): int => $x + 2)->map($mapper)->runReader(10);
 
-        expect($result)->toEqual(10);
+        expect($result)->toEqual(60);
+    });
+
+    it('apply', function () {
+        $add = static fn ($x) => static fn (int $y): int => $x + $y;
+
+        $result = Reader::reader(fn (int $x): int => $x + 2)->apply(Reader::reader($add))->runReader(10);
+
+        expect($result)->toEqual(22);
+    });
+
+    it('bind', function () {
+        $bind = static fn (int $x) => Reader::reader(fn (int $y) => $x + $y);
+
+        $result = Reader::reader(fn (int $x): int => $x + 2)->bind($bind)->runReader(10);
+
+        expect($result)->toEqual(22);
     });
 });

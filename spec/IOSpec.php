9e4b4e49cd3baf37c143170ffbdfebf6547210fa
marcosh <pasafama@gmail.com>
2022-01-28 12:35:25 +0100

    IO functor instance

--- spec/IOSpec.php
+++ spec/IOSpec.php
@@ -10,4 +10,10 @@ describe('IO', function () {
 
         expect($io->eval())->toBe(42);
     });
+
+    it('maps correctly over a function', function () {
+        $io = IO::action(fn () => 42);
+
+        expect($io->map(fn (int $i) => $i + 5)->eval())->toBe(47);
+    });
 });
--- src/IO.php
+++ src/IO.php
@@ -6,15 +6,20 @@ namespace Marcosh\LamPHPda;
 
 use Marcosh\LamPHPda\Brand\IOBrand;
 use Marcosh\LamPHPda\HK\HK1;
+use Marcosh\LamPHPda\Instances\IO\IOFunctor;
+use Marcosh\LamPHPda\Typeclass\DefaultInstance\DefaultFunctor;
+use Marcosh\LamPHPda\Typeclass\Functor;
 
 /**
  * an instance of IO<A> represents a computation which will return an instance of type A
  *
  * @template-covariant A
  *
+ * @implements DefaultFunctor<IOBrand, A>
+ *
  * @psalm-immutable
  */
-final class IO
+final class IO implements DefaultFunctor
 {
     /** @var callable(): A */
     private $action;
@@ -43,6 +48,8 @@ final class IO
      * @template B
      * @param HK1<IOBrand, B> $hk
      * @return IO<B>
+     *
+     * @psalm-pure
      */
     public static function fromBrand(HK1 $hk): self
     {
@@ -57,5 +64,26 @@ final class IO
     {
         /** @psalm-suppress ImpureFunctionCall */
         return ($this->action)();
+    }
+
+    /**
+     * @template B
+     * @param Functor<IOBrand> $functor
+     * @param callable(A): B $f
+     * @return IO<B>
+     */
+    public function imap(Functor $functor, callable $f): self
+    {
+        return self::fromBrand($functor->map($f, $this));
+    }
+
+    /**
+     * @template B
+     * @param callable(A): B $f
+     * @return IO<B>
+     */
+    public function map(callable $f): self
+    {
+        return $this->imap(new IOFunctor(), $f);
     }
 }

9756097f6d1be795fbf5d123b697f0a9793c9c28
marcosh <pasafama@gmail.com>
2022-01-28 16:22:29 +0100

    consecutive IO spec

---
+++ spec/ConsecutiveIOSpec.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+use Marcosh\LamPHPda\IO;
+
+// our model is just a `User` with an id and a name
+
+/**
+ * @psalm-immutable
+ */
+final class User
+{
+    public int $id;
+
+    public string $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
+
+// `getUser` plays the role of function which interacts with the database to retieve a `User` from its `id`
+
+/**
+ * @param int $id
+ * @return IO<User>
+ */
+function getUser(int $id): IO
+{
+    return IO::action(fn () => new User($id, 'marco'));
+}
+
+// 'retrieveRemoteData' plays the role of a network call which, given the name of the user, retrieves an integer value
+
+/**
+ * @param User $user
+ * @return IO<int>
+ */
+function retrieveRemoteData(User $user): IO
+{
+    return IO::action(fn () => strlen($user->name));
+}
+
+// we can now combine the two function calls into a single one which retrieves the integer value from the id of the user
+
+describe('IO can sequence interactions with the external world', function () {
+    it('combines getUser and retrieveRemoteData', function () {
+        $getUser = getUser(0); // no call to the database is performed here
+        $getUserAndRetrieveRemoteData = $getUser->bind('retrieveRemoteData'); // no side effect is performed here
+
+        // only when we call eval the side effects are actually performed
+        expect($getUserAndRetrieveRemoteData->eval())->toBe(5);
+    });
+});

5ba2750058128370a3f4c3311fb9fa7b8e120d7b
marcosh <pasafama@gmail.com>
2022-01-31 17:31:02 +0100

    start testing State data type

---
+++ spec/StateSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Marcosh\LamPHPda\Pair;
+use Marcosh\LamPHPda\State;
+
+describe('State', function () {
+    it('runs a stateful computation', function () {
+        $state = State::state(fn ($s) => Pair::pair($s, $s));
+
+        expect($state->runState(42))->toEqual(Pair::pair(42, 42));
+    });
+});

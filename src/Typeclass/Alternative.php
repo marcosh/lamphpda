f056dfb300a2eab8399817a3596d2e50d01659ac
Marco Perone <pasafama@gmail.com>
2023-08-17 18:04:23 +0200

    fix linting errors

--- src/Typeclass/Alternative.php
+++ src/Typeclass/Alternative.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Marcosh\LamPHPda\Typeclass;
 
 use Marcosh\LamPHPda\Brand\Brand;
-use Marcosh\LamPHPda\HK\HK1;
 
 /**
  * @see https://github.com/marcosh/lamphpda/tree/master/docs/typeclasses/Alternative.md
